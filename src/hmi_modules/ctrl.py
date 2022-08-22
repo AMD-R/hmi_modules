e0171940f503d260b362ae37493234e17cd8d7b8
ecyht2 <ecyht2@nottingham.edu.my>
2022-08-22 11:11:33 +0800

    Improved Ui_ControlWindow

--- src/hmi_modules/ctrl.py
+++ src/hmi_modules/ctrl.py
@@ -9,21 +9,11 @@ from geometry_msgs.msg import Twist
 
 
 class Ui_ControlWindow(QtWidgets.QWidget):
-    def __init__(self):
-        super().__init__()
-        self.counter = 0
-        self.timer = QtCore.QTimer()
-        self.timer_2 = QtCore.QTimer()
-        self.timer_3 = QtCore.QTimer()
-        self.timer_4 = QtCore.QTimer()
-        self.timer.timeout.connect(self.on_timeout)
-        self.timer_2.timeout.connect(self.on_timeout_2)
-        self.timer_3.timeout.connect(self.on_timeout_3)
-        self.timer_4.timeout.connect(self.on_timeout_4)
+    quitted: QtCore.PYQT_SIGNAL = QtCore.pyqtSignal()
 
-    def setupUi(self, ControlWindow):
-
-        self.ControlWindow = ControlWindow
+    def __init__(self, parent: QtWidgets.QWidget = None):
+        super().__init__(parent)
+        self.__setup_timer()
 
         self.pub = rospy.Publisher('cmd_vel', Twist, queue_size=10)
         self.rate = rospy.Rate(10)
@@ -32,26 +22,24 @@ class Ui_ControlWindow(QtWidgets.QWidget):
         self.testpub = rospy.Publisher('hmilog', String, queue_size=10)
         self.logmsg = String()
 
-        ControlWindow.setObjectName("ControlWindow")
-        ControlWindow.resize(1280, 720)
-        self.centralwidget = QtWidgets.QWidget(ControlWindow)
-        self.centralwidget.setObjectName("centralwidget")
-
-        self.myPause = QtWidgets.QPushButton(self.centralwidget)
+        # Pause Button
+        self.myPause = QtWidgets.QPushButton(self)
         self.myPause.setGeometry(QtCore.QRect(490, 240, 261, 161))
         font = QtGui.QFont()
         font.setPointSize(20)
         self.myPause.setFont(font)
         self.myPause.setObjectName("myPause")
 
-        self.myPrev = QtWidgets.QPushButton(self.centralwidget)
-        self.myPrev.setGeometry(QtCore.QRect(0, 0, 181, 81))
+        # Button to quit widget
+        self.myQuit = QtWidgets.QPushButton(self)
+        self.myQuit.setGeometry(QtCore.QRect(0, 0, 181, 81))
         font = QtGui.QFont()
         font.setPointSize(20)
-        self.myPrev.setFont(font)
-        self.myPrev.setObjectName("myPause")
+        self.myQuit.setFont(font)
+        self.myQuit.setObjectName("myQuit")
 
-        self.myForward = QtWidgets.QPushButton(self.centralwidget)
+        # Button to move AMD-R forward
+        self.myForward = QtWidgets.QPushButton(self)
         self.myForward.setGeometry(QtCore.QRect(450, 50, 341, 161))
         font = QtGui.QFont()
         font.setPointSize(20)
@@ -63,7 +51,8 @@ class Ui_ControlWindow(QtWidgets.QWidget):
         self.myForward.setIconSize(QtCore.QSize(125, 125))
         self.myForward.setObjectName("myForward")
 
-        self.myBackward = QtWidgets.QPushButton(self.centralwidget)
+        # Button to move AMD-R backward
+        self.myBackward = QtWidgets.QPushButton(self)
         self.myBackward.setGeometry(QtCore.QRect(450, 430, 341, 161))
         font = QtGui.QFont()
         font.setPointSize(20)
@@ -75,7 +64,8 @@ class Ui_ControlWindow(QtWidgets.QWidget):
         self.myBackward.setIconSize(QtCore.QSize(125, 125))
         self.myBackward.setObjectName("myBackward")
 
-        self.myRight = QtWidgets.QPushButton(self.centralwidget)
+        # Button to move AMD-R right
+        self.myRight = QtWidgets.QPushButton(self)
         self.myRight.setGeometry(QtCore.QRect(840, 240, 341, 161))
         font = QtGui.QFont()
         font.setPointSize(20)
@@ -87,21 +77,9 @@ class Ui_ControlWindow(QtWidgets.QWidget):
         self.myRight.setIconSize(QtCore.QSize(175, 175))
         self.myRight.setObjectName("myRight")
 
-        self.myLeft = QtWidgets.QPushButton(self.centralwidget)
+        # Button to move AMD-R right
+        self.myLeft = QtWidgets.QPushButton(self)
         self.myLeft.setGeometry(QtCore.QRect(60, 240, 341, 161))
-
-        self.myPause.clicked.connect(self.pause)
-        self.myPrev.clicked.connect(self.prev)
-        self.myForward.pressed.connect(self.forward)
-        self.myBackward.pressed.connect(self.backward)
-        self.myLeft.pressed.connect(self.left)
-        self.myRight.pressed.connect(self.right)
-
-        self.myForward.released.connect(self.pause)
-        self.myBackward.released.connect(self.pause)
-        self.myLeft.released.connect(self.pause)
-        self.myRight.released.connect(self.pause)
-
         font = QtGui.QFont()
         font.setPointSize(20)
         self.myLeft.setFont(font)
@@ -111,20 +89,23 @@ class Ui_ControlWindow(QtWidgets.QWidget):
         self.myLeft.setIcon(icon5)
         self.myLeft.setIconSize(QtCore.QSize(175, 175))
         self.myLeft.setObjectName("myLeft")
-        ControlWindow.setCentralWidget(self.centralwidget)
-        self.menubar = QtWidgets.QMenuBar(ControlWindow)
-        self.menubar.setGeometry(QtCore.QRect(0, 0, 1280, 26))
-        self.menubar.setObjectName("menubar")
-        ControlWindow.setMenuBar(self.menubar)
-        self.statusbar = QtWidgets.QStatusBar(ControlWindow)
-        self.statusbar.setObjectName("statusbar")
-        ControlWindow.setStatusBar(self.statusbar)
 
-        self.retranslateUi(ControlWindow)
-        QtCore.QMetaObject.connectSlotsByName(ControlWindow)
+        # Signal connection when buttons are clicked
+        self.myPause.clicked.connect(self.pause)
+        self.myQuit.clicked.connect(self.quit)
+        self.myForward.pressed.connect(self.forward)
+        self.myBackward.pressed.connect(self.backward)
+        self.myLeft.pressed.connect(self.left)
+        self.myRight.pressed.connect(self.right)
 
-    def on_timeout(self):
-        self.timer.start(100)
+        # Signal connection when buttons are released
+        self.myForward.released.connect(self.pause)
+        self.myBackward.released.connect(self.pause)
+        self.myLeft.released.connect(self.pause)
+        self.myRight.released.connect(self.pause)
+
+    def forward_timeout(self):
+        """Timer timeout to countinously move AMD-R forward when button is held down."""
         self.cmdvel.linear.x = -0.15
         self.cmdvel.linear.y = 0
         self.cmdvel.linear.z = 0
@@ -134,8 +115,8 @@ class Ui_ControlWindow(QtWidgets.QWidget):
         self.pub.publish(self.cmdvel)
         self.testpub.publish("yaaay")
 
-    def on_timeout_2(self):
-        self.timer_2.start(100)
+    def backward_timeout(self):
+        """Timer timeout to countinously move AMD-R backward when button is held down."""
         self.cmdvel.linear.x = 0.15
         self.cmdvel.linear.y = 0
         self.cmdvel.linear.z = 0
@@ -144,8 +125,8 @@ class Ui_ControlWindow(QtWidgets.QWidget):
         self.cmdvel.angular.z = 0
         self.pub.publish(self.cmdvel)
 
-    def on_timeout_3(self):
-        self.timer_3.start(100)
+    def left_timeout(self):
+        """Timer timeout to countinously move AMD-R left when button is held down."""
         self.cmdvel.linear.x = 0
         self.cmdvel.linear.y = 0
         self.cmdvel.linear.z = 0
@@ -154,8 +135,8 @@ class Ui_ControlWindow(QtWidgets.QWidget):
         self.cmdvel.angular.z = -0.25
         self.pub.publish(self.cmdvel)
 
-    def on_timeout_4(self):
-        self.timer_4.start(100)
+    def right_timeout(self):
+        """Timer timeout to countinously move AMD-R right when button is held down."""
         self.cmdvel.linear.x = 0
         self.cmdvel.linear.y = 0
         self.cmdvel.linear.z = 0
@@ -164,12 +145,13 @@ class Ui_ControlWindow(QtWidgets.QWidget):
         self.cmdvel.angular.z = 0.25
         self.pub.publish(self.cmdvel)
 
-    def prev(self):
-        print("close")
-        self.ControlWindow.close()
+    @QtCore.pyqtSlot()
+    def quit(self):
+        self.quitted.emit()
 
+    @QtCore.pyqtSlot()
     def forward(self):
-        self.timer.start(100)
+        """Starts timer to move AMD-R forward."""
         self.cmdvel.linear.x = 0
         self.cmdvel.linear.y = 0
         self.cmdvel.linear.z = 0
@@ -177,10 +159,11 @@ class Ui_ControlWindow(QtWidgets.QWidget):
         self.cmdvel.angular.y = 0
         self.cmdvel.angular.z = 0
         self.pub.publish(self.cmdvel)
-        self.timer.start(100)
+        self.forward_timer.start(100)
 
+    @QtCore.pyqtSlot()
     def backward(self):
-        self.timer.start(100)
+        """Starts timer to move AMD-R backward."""
         self.cmdvel.linear.x = 0
         self.cmdvel.linear.y = 0
         self.cmdvel.linear.z = 0
@@ -188,9 +171,11 @@ class Ui_ControlWindow(QtWidgets.QWidget):
         self.cmdvel.angular.y = 0
         self.cmdvel.angular.z = 0
         self.pub.publish(self.cmdvel)
-        self.timer_2.start(100)
+        self.backward_timer.start(100)
 
+    @QtCore.pyqtSlot()
     def left(self):
+        """Starts timer to move AMD-R left."""
         self.cmdvel.linear.x = 0
         self.cmdvel.linear.y = 0
         self.cmdvel.linear.z = 0
@@ -198,9 +183,11 @@ class Ui_ControlWindow(QtWidgets.QWidget):
         self.cmdvel.angular.y = 0
         self.cmdvel.angular.z = 0
         self.pub.publish(self.cmdvel)
-        self.timer_3.start(100)
+        self.left_timer.start(100)
 
+    @QtCore.pyqtSlot()
     def right(self):
+        """Starts timer to move AMD-R right."""
         self.cmdvel.linear.x = 0
         self.cmdvel.linear.y = 0
         self.cmdvel.linear.z = 0
@@ -208,13 +195,15 @@ class Ui_ControlWindow(QtWidgets.QWidget):
         self.cmdvel.angular.y = 0
         self.cmdvel.angular.z = 0
         self.pub.publish(self.cmdvel)
-        self.timer_4.start(100)
+        self.right_timer.start(100)
 
+    @QtCore.pyqtSlot()
     def pause(self):
-        self.timer.stop()
-        self.timer_2.stop()
-        self.timer_3.stop()
-        self.timer_4.stop()
+        """Function to stop all the timers/movement when button are released."""
+        self.forward_timer.stop()
+        self.backward_timer.stop()
+        self.left_timer.stop()
+        self.right_timer.stop()
         self.cmdvel.linear.x = 0
         self.cmdvel.linear.y = 0
         self.cmdvel.linear.z = 0
@@ -223,15 +212,16 @@ class Ui_ControlWindow(QtWidgets.QWidget):
         self.cmdvel.angular.z = 0
         self.pub.publish(self.cmdvel)
 
-    def retranslateUi(self, ControlWindow):
-        _translate = QtCore.QCoreApplication.translate
-        ControlWindow.setWindowTitle(_translate("ControlWindow", "MainWindow"))
-        self.myPrev.setText(_translate("ControlWindow", "Back"))
-        self.myPause.setText(_translate("ControlWindow", "PAUSE"))
-        # self.myForward.setText(_translate("ControlWindow", "FORWARD"))
-        # self.myBackward.setText(_translate("ControlWindow", "BACKWARD"))
-        # self.myRight.setText(_translate("ControlWindow", "RIGHT"))
-        # self.myLeft.setText(_translate("ControlWindow", "LEFT"))
+    def __setup_timer(self):
+        self.forward_timer = QtCore.QTimer()
+        self.backward_timer = QtCore.QTimer()
+        self.left_timer = QtCore.QTimer()
+        self.right_timer = QtCore.QTimer()
+
+        self.forward_timer.timeout.connect(self.forward_timeout)
+        self.backward_timer.timeout.connect(self.backward_timeout)
+        self.left_timer.timeout.connect(self.left_timeout)
+        self.right_timer.timeout.connect(self.right_timeout)
 
 
 if __name__ == "__main__":
